980eee803fff7092b64820639bd6faa6d3a5b395
Andreas Hjortland <andreas@websystemer.as>
2016-10-11 14:24:21 +0200

    Support to specify reporter explicitly from environment

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 'use strict';
 
-module.exports = (process.env.BUILD_NUMBER || process.env.TEAMCITY_VERSION) ? require('mocha-teamcity-reporter') : require('mocha').reporters.spec;
+var reporter = process.env.mocha_reporter;
+var builtInReporters = require('mocha').reporters;
+if(!reporter) {
+     // keep this for backwards compatibility
+    module.exports = (process.env.BUILD_NUMBER || process.env.TEAMCITY_VERSION) ? require('mocha-teamcity-reporter') : builtInReporters.spec;
+} else if(builtInReporters.hasOwnProperty(reporter)) {
+    module.exports = builtInReporters[reporter];
+} else {
+    module.exports = require(reporter);
+}
